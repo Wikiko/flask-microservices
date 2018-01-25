8bb0d1f11970bcdff1cb6b0c6434438892f41b69
William Azevedo <web@iscoolapp.net>
2018-01-25 15:51:07 +0000

    PART1 LESSON 7

---
+++ users-service/project/tests/base.py
@@ -0,0 +1,18 @@
+# users-service/project/tests/base.py
+
+from flask_testing import TestCase
+
+from project import app, db
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('projec.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
