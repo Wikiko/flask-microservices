5c2ad54c0bf850ad9acff29facf99c15e0a7cfc2
William Azevedo <web@iscoolapp.net>
2018-01-25 18:44:18 +0000

    Part 1, Lesson 9

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,39 +1,27 @@
 # users-service/project/__init__.py
 
-
 import os
-import datetime
-from flask import Flask, jsonify
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app():
+    # instantiate the app
+    app = Flask(__name__)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # set up extensions
+    db.init_app(app)
 
-# routes
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    from project.api.users import users_blueprint
+    # register blueprints
+    app.register_blueprint(users_blueprint)
+
+    return app
--- users-service/project/tests/base.py
+++ users-service/project/tests/base.py
@@ -2,12 +2,14 @@
 
 from flask_testing import TestCase
 
-from project import app, db
+from project import db, create_app
+
+app = create_app()
 
 
 class BaseTestCase(TestCase):
     def create_app(self):
-        app.config.from_object('projec.config.TestingConfig')
+        app.config.from_object('project.config.TestingConfig')
         return app
 
     def setUp(self):
